1f2650ef9a7be5f2528a37562f3764f07945aaf3
rengreen <r.m.gardzinska@gmail.com>
2019-02-03 21:32:50 +0100

    adding code to close hamburger menu after click event

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 var hamburger=document.querySelector(".hamburger");
+
 hamburger.addEventListener("click", function() {
     document.querySelector(".page-header").classList.toggle("nav-opened")
+}, false);
+
+var navMenuOpen=document.querySelector(".nav-menu");
+
+navMenuOpen.addEventListener("click", function() {
+    document.querySelector(".page-header").classList.remove("nav-opened")
 }, false);
